66d424b5a981e02e939c3b58d2a29f7dc333184a
Maria Istrate <mistrate06@gmail.com>
2020-05-01 16:23:21 +0300

    finalcommit

--- src/repo/AlbumRepository.java
+++ src/repo/AlbumRepository.java
@@ -1,9 +1,12 @@
 package repo;
 
 import entity.Album;
+import entity.Artist;
 import util.PersistenceUtil;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class AlbumRepository {
     public static Album findByID(int ID){
@@ -19,6 +22,21 @@ public class AlbumRepository {
         entityManager.close();
 
         return album;
+    }
+    public static void create(Album album){
+        EntityManager entityManager = PersistenceUtil.getInstance().getEntityManagerFactory().createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(album);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+
+    public static List<Album> findByName (Album album){
+        EntityManager entityManager = PersistenceUtil.getInstance().getEntityManagerFactory().createEntityManager();
+        TypedQuery albums = entityManager.createNamedQuery("AlbFindFunction",Album.class).setParameter(1, album.getName());
+        entityManager.close();
+        List<Album> albumList = albums.getResultList();
+        return albumList;
     }
 
 }
--- src/repo/ArtistRepository.java
+++ src/repo/ArtistRepository.java
@@ -1,9 +1,13 @@
 package repo;
 
+import entity.Album;
 import entity.Artist;
 import util.PersistenceUtil;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class ArtistRepository {
     public static Artist findByID(int ID){
@@ -19,6 +23,21 @@ public class ArtistRepository {
         entityManager.close();
 
         return artist;
+    }
+    public static void create(Artist artist){
+        EntityManager entityManager = PersistenceUtil.getInstance().getEntityManagerFactory().createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(artist);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+    }
+
+    public static List<Artist> findByName(Artist artist){
+        EntityManager entityManager = PersistenceUtil.getInstance().getEntityManagerFactory().createEntityManager();
+        TypedQuery artists = entityManager.createNamedQuery("ArtFindFunction",Album.class).setParameter(1, artist.getName());
+        entityManager.close();
+        List<Artist> artistList = artists.getResultList();
+        return artistList;
     }
 
 }
